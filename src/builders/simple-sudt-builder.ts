bb7eb2620d269996f7a5db5a8ec7afdfd2fd7107
classicalliu <classicalliu@gmail.com>
2020-12-29 18:14:42 +0800

    chore(simple-sudt-builder): Update capacity to 283

--- src/builders/simple-sudt-builder.ts
+++ src/builders/simple-sudt-builder.ts
@@ -44,7 +44,7 @@ export class SimpleSUDTBuilder extends Builder {
     let senderInputCKBSum = new Amount('0');
     let minSenderOccupiedCKBSum = new Amount('0');
 
-    const receiverAmount = new Amount('142');
+    const receiverAmount = new Amount('283');
     const receiverOutputCell = new Cell(
       receiverAmount,
       this.address.toLockScript(),
